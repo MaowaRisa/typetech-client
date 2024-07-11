5194d75fa7c70b84b3569e9b93f4c0fa7465c4c7
maowarisa <maowarisa123@gmail.com>
2024-07-11 14:26:24 +1000

    nav and footer done

--- src/components/layout/MainLayout.tsx
+++ src/components/layout/MainLayout.tsx
@@ -1,14 +1,21 @@
 import { Layout } from 'antd';
+import Navbar from './Navbar';
+import AppFooter from './AppFooter';
+import { Outlet } from 'react-router-dom';
 const { Header, Footer, Content } = Layout;
 
 const MainLayout = () => {
     return (
-        <div>
+        <div style={{minWidth: "375px"}}>
             <Header style={{backgroundColor: "#fff"}}>
-                
+                <Navbar></Navbar>
             </Header>
-            <Content style={{maxWidth: "1280px", margin: "auto"}}>Content</Content>
-            <Footer style={{backgroundColor: "#3B116E"}}>Footer</Footer>
+            <Content style={{maxWidth: "1280px", margin: "auto"}}>
+                <Outlet></Outlet>
+            </Content>
+            <Footer style={{backgroundColor: "#2E1065", color:"White"}}>
+                <AppFooter></AppFooter>
+            </Footer>
         </div>
     );
 };
--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -1,9 +1,65 @@
+import { MenuOutlined, ShoppingCartOutlined, UserOutlined } from "@ant-design/icons";
+import { Button } from "antd";
+
 const Navbar = () => {
-    return (
-        <div>
-            
+  return (
+    <div className="navbar">
+      <div className="navbar-start">
+        <div className="dropdown">
+          <div tabIndex={0} role="button" className="lg:hidden mr-2">
+            <MenuOutlined />
+          </div>
+          <ul
+            tabIndex={0}
+            className="menu menu-sm dropdown-content rounded-box z-[1] mt-3 w-52 p-2 shadow"
+          >
+            <li className="">
+              <a>Home</a>
+            </li>
+            <li>
+              <a href="#">Products</a>
+            </li>
+            <li>
+              <a>About Us</a>
+            </li>
+            <li>
+              <a>Contact Us</a>
+            </li>
+            <li>
+              <a>Dashboard</a>
+            </li>
+          </ul>
         </div>
-    );
+        <a className="text-xl font-bold">
+          <span className="text-violet-900">Type</span>
+          <span className="text-gray-900">Tech</span>
+        </a>
+      </div>
+      <div className="navbar-center hidden lg:flex">
+        <ul className="menu menu-horizontal px-1">
+          <li>
+            <a>Home</a>
+          </li>
+          <li>
+            <a href="#">Products</a>
+          </li>
+          <li>
+            <a>About Us</a>
+          </li>
+          <li>
+            <a>Contact Us</a>
+          </li>
+          <li>
+            <a>Dashboard</a>
+          </li>
+        </ul>
+      </div>
+      <div className="navbar-end">
+        <ShoppingCartOutlined style={{ fontSize: '1.5rem', color: '#2E1065', marginRight: '1rem' }}/>
+        <UserOutlined style={{ fontSize: '1.5rem', color: '#2E1065'}}/>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
